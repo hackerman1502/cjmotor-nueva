3ee740003185dbe833e2a354017ea2512d8fad7b
hackerman1502 <124844294+hackerman1502@users.noreply.github.com>
2025-04-17 13:44:58 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 // pages/_app.js
 import { UserProvider } from "../context/UserContext"; // Asegúrate de que la ruta sea correcta
-import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
+    <UserProvider>  {/* Envuelve la aplicación con el UserProvider */}
       <Component {...pageProps} />
     </UserProvider>
   );
