a1b6d94a74d49ab197492fe2c6812c5d4a80df2c
hackerman1502 <124844294+hackerman1502@users.noreply.github.com>
2025-04-22 10:07:16 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import { UserProvider } from "../context/UserContext";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </UserProvider>
   );
 }
