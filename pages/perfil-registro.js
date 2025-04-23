fd2ae66639c8a9b0800441cd883e8df5635f7c16
hackerman1502 <124844294+hackerman1502@users.noreply.github.com>
2025-04-23 11:57:17 +0200

    Update perfil-registro.js

--- pages/perfil-registro.js
+++ pages/perfil-registro.js
@@ -12,7 +12,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Configuración de Supabase
 const supabaseUrl = "https://ynnclpisbiyaknnoijbd.supabase.co";
-const supabaseKey = "TU_CLAVE_PUBLICA"; // ⚠️ Reemplaza por una clave pública segura
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlubmNscGlzYml5YWtubm9pamJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MjQyNDQsImV4cCI6MjA2MDQwMDI0NH0.hcPF3V32hWOT7XM0OpE0XX6cbuMDEXxvf8Ha79dT7YE"; // ⚠️ Reemplaza por una clave pública segura
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function PerfilRegistro() {
